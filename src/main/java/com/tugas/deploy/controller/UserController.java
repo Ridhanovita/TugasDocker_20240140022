fddf5bc5f88aa579592c5bd072eb287d47cade45
ctf-player <ctf-player@picoctf.net>
2026-04-14 10:19:38 +0700

    add mapping home

--- src/main/java/com/tugas/deploy/controller/UserController.java
+++ src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.tugas.deploy.controller;
 
+import com.tugas.deploy.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
+    private  final List<User> userList = new ArrayList<>();
     private final String USERNAME = "Ridha";
     private final String PASSWORD = "20240140022";
 
@@ -22,5 +28,11 @@ public class UserController {
             model.addAttribute("Error","Username atau password salah");
         }
         return "login";
+    }
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("users", userList);
+        return "home";
     }
 }
